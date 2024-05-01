444ff61950671fccfb26e6d9425b8b460d8a649c
jsxs <2261203961@qq.com>
2024-05-02 06:19:28 +0800

    first commit

---
+++ uploadGithub.bat
@@ -0,0 +1,3 @@
+git init && git add . && git commit -m "first commit" && git branch -M main && git remote add origin https://github.com/hjsdjko/xueshenggongyu.git && git push -u origin main
+
+pause
